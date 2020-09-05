ebe1b4364ae8ade2af43e0999e6691af7fc0e28f
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-09-05 22:22:56 +0530

    updated static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS=[
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT=os.path.join(BASE_DIR,'mediafiles')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 EMAIL_USE_TLS = True
